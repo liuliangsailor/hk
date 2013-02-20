d4e62784511e1308de2af116c8a5e30b1cf32b34
liuliang <liang.liu@mail.rakuten.com>
2013-02-20 14:50:32 +0800

    first commit

--- rfgrgr/src/aww.java
+++ rfgrgr/src/aww.java
@@ -11,6 +11,7 @@ System.out.println("Dfdf");
 System.out.println("d----");
 System.out.println("pppppppppppppppppppppppppppppppppppppppppppppp");
 System.out.println("mmm");
+System.out.println("894328048320482308402384023840238");
 	}
 
 }
