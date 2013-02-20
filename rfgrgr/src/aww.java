a3f246bc4a21b3b405f294120cb9d2f620876acc
liuliang <liang.liu@mail.rakuten.com>
2013-02-20 16:13:49 +0800

    parter

--- rfgrgr/src/aww.java
+++ rfgrgr/src/aww.java
@@ -6,13 +6,7 @@ public class aww {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Dfdf");
-
-System.out.println("d----");
-System.out.println("pppppppppppppppppppppppppppppppppppppppppppppp");
-System.out.println("mmm");
-System.out.println("894328048320482308402384023840238");
-System.out.println("second commit");
+System.out.println("wo shi yi ge it creazy");
 	}
 
 }
